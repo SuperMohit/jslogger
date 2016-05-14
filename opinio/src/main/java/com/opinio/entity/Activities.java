9ea7de1871ce69d9669a877a4fe5db89ba2301fb
mohit talniya <mohittalniya@gmail.com>
2016-05-15 02:21:32 +0530

    activities

---
+++ opinio/src/main/java/com/opinio/entity/Activities.java
@@ -0,0 +1,17 @@
+package com.opinio.entity;
+
+import java.util.List;
+
+public class Activities {
+	
+	private List<ActivityLog> activityLogs;
+
+	public List<ActivityLog> getActivityLogs() {
+		return activityLogs;
+	}
+
+	public void setActivityLogs(List<ActivityLog> activityLogs) {
+		this.activityLogs = activityLogs;
+	}
+
+}
